15028c641406e3fa374b5e1f0ce56d43b40b5932
Waradu <waradu@outlook.com>
2026-06-14 21:41:45 +0200

    feat: openmusicmetadata

--- scripts/generate-docs.ts
+++
@@ -1,8 +0,0 @@
-import { generateFiles } from "fumadocs-openapi";
-import { openmusicmetadata } from "./openapi";
-
-await generateFiles({
-  input: openmusicmetadata,
-  output: "./content/openmusicmetadata/endpoints",
-  includeDescription: true,
-});
